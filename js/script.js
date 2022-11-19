af5f54b576ec0d3b42dcebb66bb9df644eca766c
Ridho <syifaridhoni02@gmail.com>
2022-11-20 03:13:36 +0700

    Update web pake javascript

--- js/script.js
+++ js/script.js
@@ -8,3 +8,37 @@ document.getElementById("btns").addEventListener("click", function(){
         alert("Yahh")
     }
 });
+
+document.getElementById("btns2").addEventListener("click", function(){
+    var oke = confirm("Apakah anda ingin membeli barang ini?")
+
+   if (oke === true){
+       var oke = prompt("Masukkan jumlah barang yang ingin dibeli")
+       alert("Pesanan anda sebanyak " + oke + " buah akan segera diproses")
+   } else {
+       alert("Yahh")
+   }
+});
+
+document.getElementById("btns3").addEventListener("click", function(){
+    var oke = confirm("Apakah anda ingin membeli barang ini?")
+
+   if (oke === true){
+       var oke = prompt("Masukkan jumlah barang yang ingin dibeli")
+       alert("Pesanan anda sebanyak " + oke + " buah akan segera diproses")
+   } else {
+       alert("Yahh")
+   }
+});
+
+document.getElementById("btns4").addEventListener("click", function(){
+    var oke = confirm("Apakah anda ingin membeli barang ini?")
+
+   if (oke === true){
+       var oke = prompt("Masukkan jumlah barang yang ingin dibeli")
+       alert("Pesanan anda sebanyak " + oke + " buah akan segera diproses")
+   } else {
+       alert("Yahh")
+   }
+});
+
