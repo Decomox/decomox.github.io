8ee478a4aee4718b170a28a5bea42b7703f85539
Ridho <syifaridhoni02@gmail.com>
2022-11-20 12:20:20 +0700

    Update web pada bagian nama produk dan deskripsi

--- js/script.js
+++ js/script.js
@@ -1,42 +1,42 @@
 document.getElementById("btns").addEventListener("click", function(){
-     var oke = confirm("Apakah anda ingin membeli barang ini?")
+     var oke = confirm("Apakah anda ingin membeli Endaru Authentic Bag?")
 
     if (oke === true){
         var oke = prompt("Masukkan jumlah barang yang ingin dibeli")
-        alert("Pesanan anda sebanyak " + oke + " buah akan segera diproses")
+        alert("Pesanan anda sebanyak " + oke + " buah Endaru Authentic Bag akan segera diproses")
     } else {
         alert("Yahh")
     }
 });
 
 document.getElementById("btns2").addEventListener("click", function(){
-    var oke = confirm("Apakah anda ingin membeli barang ini?")
+    var oke = confirm("Apakah anda ingin membeli Djani Authentic Shoes?")
 
    if (oke === true){
        var oke = prompt("Masukkan jumlah barang yang ingin dibeli")
-       alert("Pesanan anda sebanyak " + oke + " buah akan segera diproses")
+       alert("Pesanan anda sebanyak " + oke + " buah Djani Authentic Shoes akan segera diproses")
    } else {
        alert("Yahh")
    }
 });
 
 document.getElementById("btns3").addEventListener("click", function(){
-    var oke = confirm("Apakah anda ingin membeli barang ini?")
+    var oke = confirm("Apakah anda ingin membeli Praba Authentic Tumblr?")
 
    if (oke === true){
        var oke = prompt("Masukkan jumlah barang yang ingin dibeli")
-       alert("Pesanan anda sebanyak " + oke + " buah akan segera diproses")
+       alert("Pesanan anda sebanyak " + oke + " buah Praba Authentic Tumblr akan segera diproses")
    } else {
        alert("Yahh")
    }
 });
 
 document.getElementById("btns4").addEventListener("click", function(){
-    var oke = confirm("Apakah anda ingin membeli barang ini?")
+    var oke = confirm("Apakah anda ingin membeli Taraka Authentic Tshirt?")
 
    if (oke === true){
        var oke = prompt("Masukkan jumlah barang yang ingin dibeli")
-       alert("Pesanan anda sebanyak " + oke + " buah akan segera diproses")
+       alert("Pesanan anda sebanyak " + oke + " buah Taraka Authentic Tshirt akan segera diproses")
    } else {
        alert("Yahh")
    }
